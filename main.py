b34fcae4122d1a48dd3a96b8fbca2ae29ccf6d25
yumetia <yumetia000@gmail.com>
2025-03-02 14:39:53 +0100

    game over timer, stones intervals short like raining added

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import pyxel
 
 SCREEN_WIDTH = 160
 SCREEN_HEIGHT = 120
+STONE_INTERVAL = 5
+
+
+class Stone:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    
+    def update(self):
+        self.y+=2
+    def draw(self):
+        # 石
+        pyxel.blt(self.x,self.y,0,8,0,8,8,pyxel.COLOR_BLACK)
+
 
 class App:
     def __init__(self):
@@ -11,9 +25,10 @@ class App:
 
         self.player_x = SCREEN_WIDTH // 2
         self.player_y = SCREEN_HEIGHT*4//5
-        self.stone_x = SCREEN_WIDTH//2
-        self.stone_y = 0
+        
+        self.stones=[]
         self.is_colliding = False
+        self.game_over_timer = 0
 
         pyxel.run(self.update,self.draw)
 
@@ -30,26 +45,36 @@ class App:
             self.player_y -=1
         elif pyxel.btn(pyxel.KEY_DOWN):
             self.player_y +=1
-            
-        print("stone_x:",self.stone_x)
-        print("player_x:",self.player_x)
-        print("stone_y:",self.stone_y)
-        print("player_y:",self.player_y)
+        
+        if pyxel.frame_count%STONE_INTERVAL==0:
+            self.stones.append(Stone(pyxel.rndi(0,SCREEN_WIDTH-6),0))
 
-        # colliding stone/player ?
-        if (self.player_x-4 <= self.stone_x <= self.player_x+4) and (self.player_y-4 <= self.stone_y <= self.player_y-4):
-            self.is_colliding=True
-        # stone
-        self.stone_y+=1
+        for stone in self.stones.copy():
+            stone.update()
+
+            # colliding stone/player ?
+            if (self.player_x <= stone.x <= self.player_x+8) and (self.player_y <= stone.y <= self.player_y+8):
+                self.is_colliding=True
+            
+            if stone.y>=SCREEN_HEIGHT:
+                self.stones.remove(stone)
+            print(len(self.stones))
+
 
     def draw(self):
-        pyxel.cls(pyxel.COLOR_DARK_BLUE)
-        # 石
-        pyxel.blt(self.stone_x,self.stone_y,0,8,0,8,8,pyxel.COLOR_BLACK)
+        pyxel.cls(pyxel.COLOR_GREEN)
+
+        # stones
+        for stone in self.stones:
+            stone.draw()
+
         # player
         pyxel.blt(self.player_x,self.player_y,0,16,0,16,16,pyxel.COLOR_BLACK)
         # game over
-        if self.is_colliding==True:
+        if self.is_colliding:
+            self.game_over_timer+=1
             pyxel.text(SCREEN_WIDTH//2,SCREEN_HEIGHT//2,"GAME OVER",pyxel.COLOR_RED)
+            if self.game_over_timer>30:
+                pyxel.quit()
 
 App()
